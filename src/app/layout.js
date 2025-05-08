6a46650b60e638332021046a436fcc7200a83e20
joaoporto27 <joao.v.sales15@aluno.senai.br>
2025-05-08 10:50:43 -0300

    Feat: Teste teste teste

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,17 @@
 import "./globals.css";
 
 export const metadata = {
-    title: "My Exam Mockup",
+    title:"Filtro de Países",
+    icons: {
+        icon: "/icons/favicon.ico",
+    },
     description: "Projeto pra mostrar tudo que eu sei",
+
 };
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="pt-BR">
+        <html>
             <body>{children}</body>
         </html>
     );
